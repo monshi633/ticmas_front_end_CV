3ac10efcdf946a9c0a329d1b1003c008a6956d16
Simon <monshi.633@gmail.com>
2023-01-21 01:15:22 +0100

    Consolidates website HTML and CSS, ready for last check-up and code cleansing

--- 03_Proyecto_final_integrador/functions.js
+++ 03_Proyecto_final_integrador/functions.js
@@ -44,20 +44,6 @@ document.getElementById('section3').addEventListener('click', function() {
     document.getElementById('section3').style.color = 'var(--color-accent2)';
 });
 
-// document.getElementById('section4').addEventListener('click', function() {
-//     const collection = document.getElementsByClassName('card');
-//     for (let i = 0; i < collection.length; i++) {
-//         collection[i].style.display = 'None';
-//     }
-//     document.getElementById('tools').style.display = 'block';
-
-//     const items = document.getElementsByClassName('item');
-//     for (let i = 0; i < items.length; i++) {
-//         items[i].style.color = 'var(--color-text)';
-//     }
-//     document.getElementById('section4').style.color = 'var(--color-accent2)';
-// });
-
 // Auto-calculate age
 var dob = document.getElementById('birthdate').textContent;
 document.getElementById('age').innerHTML = getAge(dob);
